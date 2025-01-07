2544a641850ab71573df25fd361f9a4f46b46178
Pavaillon <alexandrepavaillon@gmail.com>
2025-01-07 17:55:57 +0100

    Ajout de la fonctionnalité display_quotes() avec gestion de count

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
 from functions import *
 
 def menu():
-    print("\n==== Programming Quotes ====")
-    print("1. Random quote")
-    print("2. All quotes")
+    print("1. Display a random quote")
+    print("2. Display multiple quotes")
     print("3. Exit")
 
 def main():
+    quotes = load_quotes()  # Fonction qui charge les citations
     while True:
-        quotes = load_quotes("quotes.txt")
         menu()
-
-        choice = input("Choose your an action (1-3): ")
-        
+        choice = input("Enter your choice: ")
         if choice == "1":
-            print_quote(random_quote(quotes))
-        elif choice == "2":
-            view_quotes(quotes)
+            display_quote(quotes)
+        elif choice == "2":  # Gestion de l'affichage de plusieurs citations
+            count = int(input("Enter the number of quotes to display: "))
+            display_quotes(quotes, count)
         elif choice == "3":
-            print("Good bye...")
+            print("Goodbye!")
             break
         else:
-            print("Invalid input")
+            print("Invalid choice. Please try again.")
 
 if __name__ == "__main__":
     main()
